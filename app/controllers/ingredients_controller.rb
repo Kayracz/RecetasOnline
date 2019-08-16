10a0fdea250b03ea2f349877dd735d48a7a6e0b3
Kayra Czerniewicz <kayracz@gmail.com>
2019-08-16 14:51:32 -0400

    fixed bugs in ingredients and directions stylesheets

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -29,10 +29,8 @@ class IngredientsController < ApplicationController
     respond_to do |format|
       if @ingredient.save
         format.html { redirect_to @ingredient, notice: 'Ingredient was successfully created.' }
-        format.json { render :show, status: :created, location: @ingredient }
       else
         format.html { render :new }
-        format.json { render json: @ingredient.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,10 +41,8 @@ class IngredientsController < ApplicationController
     respond_to do |format|
       if @ingredient.update(ingredient_params)
         format.html { redirect_to @ingredient, notice: 'Ingredient was successfully updated.' }
-        format.json { render :show, status: :ok, location: @ingredient }
       else
         format.html { render :edit }
-        format.json { render json: @ingredient.errors, status: :unprocessable_entity }
       end
     end
   end
